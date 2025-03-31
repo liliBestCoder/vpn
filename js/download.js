ed3c879bbf200f9eba2afdce691b56a9b4839ae5
quxiaojun <quxiaojun@anxinsec.com>
2025-03-31 21:35:19 +0800

    update download

---
+++ js/download.js
@@ -0,0 +1,22 @@
+async function downloadFile(urls, filename) {
+    const fileStream = streamSaver.createWriteStream(filename);
+    const writer = fileStream.getWriter();
+
+    for (const url of urls) {
+        const response = await fetch(url);
+        const reader = response.body.getReader();
+        const chunks = [];
+
+        while (true) {
+            const { done, value } = await reader.read();
+            if (done) break;
+            chunks.push(value);
+        }
+
+        for (const chunk of chunks) {
+            await writer.write(chunk);
+        }
+    }
+
+    writer.close();
+}
